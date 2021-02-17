5b878b80dc7bd61c74eea216479afbaff728957f
Sean Mullarkey <seanmullarkey@ip-192-168-0-22.us-west-2.compute.internal>
2021-02-16 19:24:49 -0700

    removed sensitive files

--- run_tester.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source_env() {
-  [ "$#" -eq 1 ] && env="$1" || env=".env"
-  [ -f "$env" ] || { echo "Env file $env doesn't exist"; return 1; }
-  eval $(grep -Ev '^#|^$' "$env" | sed -e 's/=\(.*\)/="\1/g' -e 's/$/"/g' -e 's/^/export /')
-}
-source_env
-
-python3 ./exampleTest/example_tester.py $TECH_LAB_BOT_ID ODExMzc1MzUwNzk3Njk3MDU0.YCxSLg.oMhShjAIqxUH9V3IB7qfrCEDvSs -c $CHANNEL_ID -r all
