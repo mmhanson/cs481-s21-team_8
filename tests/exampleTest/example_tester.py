e261f00a35efdacb4ff88e7c58c9ac11ec846fe9
Sean Mullarkey <seanmullarkey@ip-192-168-0-22.us-west-2.compute.internal>
2021-03-04 09:13:02 -0700

    temporary log out function. Both bots will log out after tests are ran

--- tests/exampleTest/example_tester.py
+++ tests/exampleTest/example_tester.py
@@ -125,10 +125,12 @@ async def test_reply_on_edit(interface):
     await interface.edit_message(message, "Say 'Yeah, that is cool!'")
     await interface.assert_message_contains(message, "Yeah, that is cool!")
 
-# @test_collector()
-# async def test_reply_on_log_out(interface):
-#     await asyncio.sleep(1)
-#     await interface.assert_reply_contains("log out", "logging off...")
+# log out after tests
+@test_collector()
+async def test_log_out(interface):
+    message = await interface.send_message("log out")
+    # await asyncio.sleep(1)
+    # await interface.assert_reply_contains(message, "logging off...")
 
 # Actually run the bot
 
